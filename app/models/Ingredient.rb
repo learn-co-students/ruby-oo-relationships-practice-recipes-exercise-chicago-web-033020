43c9b20eaf5d96d92bf1f8fcd20d8d72d22b15d2
mack_mcquen <mackmcquen@comcast.net>
2020-04-06 21:40:57 -0500

    Done

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,11 @@
+class Ingredient
+    attr_reader :title
+    
+    def initialize(title)
+        @title = title
+    end
 
+    def to_s
+        "I'm #{title}! I'm delicious!"
+    end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,34 @@
 require_relative '../config/environment.rb'
 
+def reload
+    load 'config/environment.rb'
+  end
+
+cake = Recipe.new ("Birthday Cake")
+donut = Recipe.new ("Pink Donut")
+burger = Recipe.new ("Cheeseburger")
+noodles = Recipe.new ("Spaggheti")
+
+al = User.new("Al")
+pat = User.new("Pat")
+sandy = User.new("Sandy")
+dana = User.new("Dana")
+
+batter = Ingredient.new("batter")
+chocolate = Ingredient.new("chocolate")
+sprinkles = Ingredient.new("sprinkles")
+sugar = Ingredient.new("sugar")
+
+donut.add_ingredients([batter, chocolate, sprinkles, sugar])
+p RecipeIngredient.all
+puts donut.ingredients
+
+pat.add_recipe_card(cake, "03/31/20", "5 turnips!")
+pat.add_recipe_card(donut, "03/31/20", "5 turnips!")
+pat.add_recipe_card(burger, "03/31/20", "5 turnips!")
+sandy.add_recipe_card(donut, "03/31/20", "5 turnips!")
+dana.add_recipe_card(donut, "03/31/20", "5 turnips!")
+dana.add_recipe_card(noodles, "03/31/20", "5 turnips!")
+al.add_recipe_card(cake, "04/01/20", "It's delicious!")
+
 binding.pry
